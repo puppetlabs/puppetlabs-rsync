2f0aa7687fa59968d8858afb05007c1f1bc442e5
Andreas de Pretis <ad@25th-floor.com>
2015-09-16 18:36:34 +0200

    Add support for module 'log file'

--- manifests/server/module.pp
+++ manifests/server/module.pp
@@ -25,6 +25,7 @@
 #    for logging file transfers when transfer logging is enabled. See the 
 #    rsyncd.conf documentation for more details.
 #   $refuse_options   - list of rsync command line options that will be refused by your rsync daemon.
+#   $log_file         - log messages to the indicated file rather than using syslog
 #
 #   sets up an rsync server
 #
@@ -58,7 +59,8 @@ define rsync::server::module (
   $hosts_deny       = undef,
   $transfer_logging = undef,
   $log_format       = undef,
-  $refuse_options   = undef)  {
+  $refuse_options   = undef,
+  $log_file         = undef)  {
 
   concat::fragment { "frag-${name}":
     content => template('rsync/module.erb'),

eebe1f6531c1207895aa212e2084e1b80ce9eff6
Pat Riehecky <riehecky@fnal.gov>
2017-05-12 11:45:18 -0500

    Added missing module options

--- spec/defines/server_module_spec.rb
+++ spec/defines/server_module_spec.rb
@@ -33,12 +33,15 @@ describe 'rsync::server::module', :type => :define do
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^path\s*=\s*\/some\/path$/) }
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^read only\s*=\s*yes$/) }
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^write only\s*=\s*no$/) }
+    it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^use chroot\s*=\s*yes$/) }
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^list\s*=\s*yes$/) }
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^uid\s*=\s*0$/) }
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^gid\s*=\s*0$/) }
+    it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^numeric ids\s*=\s*yes$/) }
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^incoming chmod\s*=\s*0644$/) }
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^outgoing chmod\s*=\s*0644$/) }
     it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^max connections\s*=\s*0$/) }
+    it { is_expected.to contain_concat__fragment(fragment_name).with_content(/^timeout\s*=\s*0$/) }
     it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^lock file\s*=.*$/) }
     it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^secrets file\s*=.*$/) }
     it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^auth users\s*=.*$/) }
@@ -47,6 +50,11 @@ describe 'rsync::server::module', :type => :define do
     it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^transfer logging\s*=.*$/) }
     it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^log format\s*=.*$/) }
     it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^refuse options\s*=.*$/) }
+    it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^include\s*=.*$/) }
+    it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^include from\s*=.*$/) }
+    it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^exclude\s*=.*$/) }
+    it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^exclude from\s*=.*$/) }
+    it { is_expected.not_to contain_concat__fragment(fragment_name).with_content(/^dont compress\s*=.*$/) }
   end
 
   describe "when overriding max connections" do
@@ -71,17 +79,26 @@ describe 'rsync::server::module', :type => :define do
     :comment            => 'super module !',
     :read_only          => 'no',
     :write_only         => 'yes',
+    :use_chroot         => 'no',
     :list               => 'no',
     :uid                => '4682',
     :gid                => '4682',
+    :numeric_ids        => 'no',
     :incoming_chmod     => '0777',
     :outgoing_chmod     => '0777',
+    :max_connections    => '10',
+    :timeout            => '10',
     :secrets_file       => '/path/to/secrets',
     :hosts_allow        => ['localhost', '169.254.42.51'],
     :hosts_deny         => ['some-host.example.com', '10.0.0.128'],
     :transfer_logging   => 'true',
     :log_format         => '%t %a %m %f %b',
     :refuse_options     => ['c', 'delete'],
+    :include            => ['a', 'b'],
+    :exclude            => ['c', 'd'],
+    :include_from       => '/path/to/file',
+    :exclude_from       => '/path/to/otherfile',
+    :dont_compress      => ['a', 'b'],
     :ignore_nonreadable => 'yes'
   }.each do |k,v|
     describe "when overriding #{k}" do

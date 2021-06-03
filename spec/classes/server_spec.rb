6decded210ec22e33fd0403c2223f67af4fe3d91
Chad Gorshing <gorshing@gmail.com>
2021-06-03 17:09:07 -0500

    Correcte rubocop style checking errors

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-describe 'rsync::server', :type => :class do
+describe 'rsync::server', type: :class do
   on_supported_os.each do |os, facts|
     context "on #{os} " do
       let :facts do
@@ -9,21 +9,21 @@ describe 'rsync::server', :type => :class do
       describe 'when using default params' do
         it {
           is_expected.to contain_class('xinetd')
-          is_expected.to contain_xinetd__service('rsync').with({ 'bind' => '0.0.0.0' })
+          is_expected.to contain_xinetd__service('rsync').with(bind: '0.0.0.0')
           is_expected.not_to contain_service('rsync')
           is_expected.not_to contain_file('/etc/rsync-motd')
-          is_expected.to contain_concat__fragment('rsyncd_conf_header').with({
-            :order => '00_header',
-          })
-          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(/^use chroot\s*=\s*yes$/)
-          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(/^address\s*=\s*0.0.0.0$/)
-          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(/^pid file\s*=\s*\/var\/run\/rsyncd.pid$/)
+          is_expected.to contain_concat__fragment('rsyncd_conf_header').with(
+            order: '00_header',
+          )
+          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(%r{^use chroot\s*=\s*yes$})
+          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(%r{^address\s*=\s*0.0.0.0$})
+          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(%r{^pid file\s*=\s*\/var\/run\/rsyncd.pid$})
         }
       end
 
       describe 'when disabling xinetd' do
         let :params do
-          { :use_xinetd => false }
+          { use_xinetd: false }
         end
 
         it {
@@ -31,17 +31,17 @@ describe 'rsync::server', :type => :class do
           is_expected.not_to contain_xinetd__service('rsync')
         }
         servicename = case facts[:os]['family']
-        when 'RedHat', 'Suse', 'FreeBSD'
-          'rsyncd'
-        else
-          'rsync'
-        end
+                      when 'RedHat', 'Suse', 'FreeBSD'
+                        'rsyncd'
+                      else
+                        'rsync'
+                      end
         it { is_expected.to contain_service(servicename) }
       end
 
       describe 'when setting an motd' do
         let :params do
-          { :motd_file => true }
+          { motd_file: true }
         end
 
         it {
@@ -51,51 +51,51 @@ describe 'rsync::server', :type => :class do
 
       describe 'when unsetting pid file' do
         let :params do
-          { :pid_file => 'UNSET' }
+          { pid_file: 'UNSET' }
         end
 
         it {
-          is_expected.not_to contain_concat__fragment('rsyncd_conf_header').with_content(/^pid file\s*=/)
+          is_expected.not_to contain_concat__fragment('rsyncd_conf_header').with_content(%r{^pid file\s*=})
         }
       end
 
       describe 'when overriding use_chroot' do
         let :params do
-          { :use_chroot => 'no' }
+          { use_chroot: 'no' }
         end
 
         it {
-          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(/^use chroot\s*=\s*no$/)
+          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(%r{^use chroot\s*=\s*no$})
         }
       end
 
       describe 'when overriding address' do
         let :params do
-          { :address => '10.0.0.42' }
+          { address: '10.0.0.42' }
         end
 
         it {
-          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(/^address\s*=\s*10.0.0.42$/)
+          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(%r{^address\s*=\s*10.0.0.42$})
         }
       end
 
       describe 'when overriding uid' do
         let :params do
-          { :uid => 'testuser' }
+          { uid: 'testuser' }
         end
 
         it {
-          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(/^uid\s*=\s*testuser$/)
+          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(%r{^uid\s*=\s*testuser$})
         }
       end
 
       describe 'when overriding gid' do
         let :params do
-          { :gid => 'testgroup' }
+          { gid: 'testgroup' }
         end
 
         it {
-          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(/^gid\s*=\s*testgroup$/)
+          is_expected.to contain_concat__fragment('rsyncd_conf_header').with_content(%r{^gid\s*=\s*testgroup$})
         }
       end
     end
--- spec/defines/get_spec.rb
+++ spec/defines/get_spec.rb
@@ -1,322 +1,321 @@
 require 'spec_helper'
-describe 'rsync::get', :type => :define do
+describe 'rsync::get', type: :define do
   let :title do
     'foobar'
   end
 
   let :common_params do
     {
-      :source => 'example.com'
+      source: 'example.com',
     }
   end
 
-  describe "when using default class paramaters" do
+  describe 'when using default class paramaters' do
     let :params do
       common_params
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a example.com foobar | wc -l` -gt 0",
-        'timeout' => '900',
-        'user'    => 'root'
-      })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a example.com foobar',
+        onlyif: 'test `rsync --dry-run --itemize-changes -a example.com foobar | wc -l` -gt 0',
+        timeout: '900',
+        user: 'root',
+      )
     }
   end
 
-  describe "when setting the execuser" do
+  describe 'when setting the execuser' do
     let :params do
-      common_params.merge( { :execuser => 'username' } )
+      common_params.merge(execuser: 'username')
     end
 
-    it{ is_expected.to contain_exec("rsync foobar").with({ 'user' => 'username' }) }
+    it { is_expected.to contain_exec('rsync foobar').with(user: 'username') }
   end
 
-  describe "when setting the timeout" do
+  describe 'when setting the timeout' do
     let :params do
-      common_params.merge( { :timeout => '200' } )
-    end
-
-    it {
-      is_expected.to contain_exec("rsync foobar").with({ 'timeout' => '200' })
-    }
-  end
-
-  describe "when setting a user but not a keyfile" do
-    let :params do
-      common_params.merge({ :user => 'mr_baz' })
+      common_params.merge(timeout: '200')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a -e \'ssh -i /home/mr_baz/.ssh/id_rsa -l mr_baz\' mr_baz@example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a -e \'ssh -i /home/mr_baz/.ssh/id_rsa -l mr_baz\' mr_baz@example.com foobar | wc -l` -gt 0",
-      })
+      is_expected.to contain_exec('rsync foobar').with(timeout: '200')
     }
   end
 
-  describe "when setting a keyfile but not a user" do
+  describe 'when setting a user but not a keyfile' do
     let :params do
-      common_params.merge( { :keyfile => "/path/to/keyfile" } )
+      common_params.merge(user: 'mr_baz')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a example.com foobar | wc -l` -gt 0",
-      })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a -e \'ssh -i /home/mr_baz/.ssh/id_rsa -l mr_baz\' mr_baz@example.com foobar',
+        onlyif:  "test `rsync --dry-run --itemize-changes -a -e \'ssh -i /home/mr_baz/.ssh/id_rsa -l mr_baz\' mr_baz@example.com foobar | wc -l` -gt 0",
+      )
     }
   end
 
-  describe "when setting a user and a keyfile" do
+  describe 'when setting a keyfile but not a user' do
     let :params do
-      common_params.merge({
-        :user    => 'mr_baz',
-        :keyfile => '/path/to/keyfile'
-      })
+      common_params.merge(keyfile: '/path/to/keyfile')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a -e \'ssh -i /path/to/keyfile -l mr_baz\' mr_baz@example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a -e \'ssh -i /path/to/keyfile -l mr_baz\' mr_baz@example.com foobar | wc -l` -gt 0",
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when setting an exclude path" do
+  describe 'when setting a user and a keyfile' do
     let :params do
-      common_params.merge({ :exclude => '/path/to/exclude/' })
+      common_params.merge(
+        user:    'mr_baz',
+        keyfile: '/path/to/keyfile',
+      )
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --exclude=/path/to/exclude/ example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --exclude=/path/to/exclude/ example.com foobar | wc -l` -gt 0",
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a -e \'ssh -i /path/to/keyfile -l mr_baz\' mr_baz@example.com foobar',
+        onlyif: 'test `rsync --dry-run --itemize-changes -a -e \'ssh -i /path/to/keyfile -l mr_baz\' mr_baz@example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when setting multiple exclude paths" do
+  describe 'when setting an exclude path' do
     let :params do
-      common_params.merge({ :exclude => ['logs/', 'tmp/'] })
+      common_params.merge(exclude: '/path/to/exclude/')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --exclude=logs/ --exclude=tmp/ example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --exclude=logs/ --exclude=tmp/ example.com foobar | wc -l` -gt 0",
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --exclude=/path/to/exclude/ example.com foobar',
+        onlyif: 'test `rsync --dry-run --itemize-changes -a --exclude=/path/to/exclude/ example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when setting an include path" do
+  describe 'when setting multiple exclude paths' do
     let :params do
-      common_params.merge({ :include => '/path/to/include/' })
+      common_params.merge(exclude: ['logs/', 'tmp/'])
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --include=/path/to/include/ example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --include=/path/to/include/ example.com foobar | wc -l` -gt 0",
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --exclude=logs/ --exclude=tmp/ example.com foobar',
+        onlyif: 'test `rsync --dry-run --itemize-changes -a --exclude=logs/ --exclude=tmp/ example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when setting multiple include paths" do
+  describe 'when setting an include path' do
     let :params do
-      common_params.merge({ :include => [ 'htdocs/', 'cache/' ] })
+      common_params.merge(include: '/path/to/include/')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --include=htdocs/ --include=cache/ example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --include=htdocs/ --include=cache/ example.com foobar | wc -l` -gt 0",
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --include=/path/to/include/ example.com foobar',
+        onlyif: 'test `rsync --dry-run --itemize-changes -a --include=/path/to/include/ example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when leaving default exclude and include order (exclude first)" do
+  describe 'when setting multiple include paths' do
     let :params do
-      common_params.merge({ :include => 'cache/' , :exclude => 'something' })
+      common_params.merge(include: ['htdocs/', 'cache/'])
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --exclude=something --include=cache/ example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --exclude=something --include=cache/ example.com foobar | wc -l` -gt 0",
-      })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --include=htdocs/ --include=cache/ example.com foobar',
+        onlyif: 'test `rsync --dry-run --itemize-changes -a --include=htdocs/ --include=cache/ example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when changing the exclude and include order to include first" do
+  describe 'when leaving default exclude and include order (exclude first)' do
     let :params do
-      common_params.merge({ :include => 'cache/' , :exclude => 'something', :exclude_first => false })
+      common_params.merge(include: 'cache/', exclude: 'something')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --include=cache/ --exclude=something example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --include=cache/ --exclude=something example.com foobar | wc -l` -gt 0",
-      })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --exclude=something --include=cache/ example.com foobar',
+        onlyif: 'test `rsync --dry-run --itemize-changes -a --exclude=something --include=cache/ example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when enabling purge" do
+  describe 'when changing the exclude and include order to include first' do
     let :params do
-      common_params.merge({ :purge => true })
+      common_params.merge(include: 'cache/', exclude: 'something', exclude_first: false)
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --delete example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --delete example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --include=cache/ --exclude=something example.com foobar',
+        onlyif: 'test `rsync --dry-run --itemize-changes -a --include=cache/ --exclude=something example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when enabling recursive" do
+  describe 'when enabling purge' do
     let :params do
-      common_params.merge({ :recursive => true })
+      common_params.merge(purge: true)
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a -r example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a -r example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --delete example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a --delete example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when enabling links" do
+  describe 'when enabling recursive' do
     let :params do
-      common_params.merge({ :links => true })
+      common_params.merge(recursive: true)
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --links example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --links example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a -r example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a -r example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when changing rsync options" do
+  describe 'when enabling links' do
     let :params do
-      common_params.merge({ :options => '-rlpcgoD' })
+      common_params.merge(links: true)
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -rlpcgoD example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -rlpcgoD example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --links example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a --links example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when enabling hardlinks" do
+  describe 'when changing rsync options' do
     let :params do
-      common_params.merge({ :hardlinks => true })
+      common_params.merge(options: '-rlpcgoD')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --hard-links example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --hard-links example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -rlpcgoD example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -rlpcgoD example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when enabling copylinks" do
+  describe 'when enabling hardlinks' do
     let :params do
-      common_params.merge({ :copylinks => true })
+      common_params.merge(hardlinks: true)
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --copy-links example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --copy-links example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --hard-links example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a --hard-links example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when enabling times" do
+  describe 'when enabling copylinks' do
     let :params do
-      common_params.merge({ :times => true })
+      common_params.merge(copylinks: true)
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --times example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --times example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --copy-links example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a --copy-links example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when setting a custom path" do
+  describe 'when enabling times' do
     let :params do
-      common_params.merge({ :path => 'barfoo' })
+      common_params.merge(times: true)
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a example.com barfoo',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a example.com barfoo | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --times example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a --times example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when setting a custom onlyif condition" do
+  describe 'when setting a custom path' do
     let :params do
-      common_params.merge({ :onlyif => 'false' })
+      common_params.merge(path: 'barfoo')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a example.com foobar',
-        'onlyif'  => "false"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a example.com barfoo',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a example.com barfoo | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when setting chown" do
+  describe 'when setting a custom onlyif condition' do
     let :params do
-      common_params.merge({ :chown => 'user:group' })
+      common_params.merge(onlyif: 'false')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --chown=user:group example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --chown=user:group example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a example.com foobar',
+        onlyif:  'false',
+      )
     }
   end
 
-  describe "when setting chmod" do
+  describe 'when setting chown' do
     let :params do
-      common_params.merge({ :chmod => 'Dg-s,u+w,go-w,+X,+x' })
+      common_params.merge(chown: 'user:group')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --chmod=Dg-s,u+w,go-w,+X,+x example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --chmod=Dg-s,u+w,go-w,+X,+x example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --chown=user:group example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a --chown=user:group example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
-  describe "when setting logfile" do
+  describe 'when setting chmod' do
     let :params do
-      common_params.merge({ :logfile => '/tmp/logfile.out' })
+      common_params.merge(chmod: 'Dg-s,u+w,go-w,+X,+x')
     end
 
     it {
-      is_expected.to contain_exec("rsync foobar").with({
-        'command' => 'rsync -q -a --log-file=/tmp/logfile.out example.com foobar',
-        'onlyif'  => "test `rsync --dry-run --itemize-changes -a --log-file=/tmp/logfile.out example.com foobar | wc -l` -gt 0"
-       })
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --chmod=Dg-s,u+w,go-w,+X,+x example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a --chmod=Dg-s,u+w,go-w,+X,+x example.com foobar | wc -l` -gt 0',
+      )
     }
   end
 
+  describe 'when setting logfile' do
+    let :params do
+      common_params.merge(logfile: '/tmp/logfile.out')
+    end
+
+    it {
+      is_expected.to contain_exec('rsync foobar').with(
+        command: 'rsync -q -a --log-file=/tmp/logfile.out example.com foobar',
+        onlyif:  'test `rsync --dry-run --itemize-changes -a --log-file=/tmp/logfile.out example.com foobar | wc -l` -gt 0',
+      )
+    }
+  end
 end
